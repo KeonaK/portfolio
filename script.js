54d5c123dd1b917b916092c82f49235758f96514
Keona Kirby <keonakirby@gmail.com>
2021-06-17 14:29:44 -0500

    Add/keyframes animation for overlay to fade in and out when hamburgerbtn is clicked

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const hamburgerBtn = document.querySelector("#hamburgerBtn");
-header = document.querySelector(".header");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
 
 hamburgerBtn.addEventListener("click", () => {
   // check if the header has the open class
+  //closes menu
   if (header.classList.contains("open")) {
     header.classList.remove("open");
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+    //open menu
   } else {
     header.classList.add("open");
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
   }
 });
