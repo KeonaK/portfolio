3a13b228bdcb92de8722d2f0925e9980dc9190ca
Keona Kirby <keonakirby@gmail.com>
2021-06-17 21:02:12 -0500

    Update/hamburger menu is now working and is clickable

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 const hamburgerBtn = document.querySelector("#hamburgerBtn");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+// creates an array
+const hasFade = document.querySelectorAll(".has-fade");
 
 hamburgerBtn.addEventListener("click", () => {
   // check if the header has the open class
   //closes menu
   if (header.classList.contains("open")) {
     header.classList.remove("open");
-    overlay.classList.remove("fade-in");
-    overlay.classList.add("fade-out");
+    hasFade.forEach((i) => {
+      i.classList.remove("fade-in");
+      i.classList.add("fade-out");
+    });
     //open menu
   } else {
     header.classList.add("open");
-    overlay.classList.remove("fade-out");
-    overlay.classList.add("fade-in");
+    hasFade.forEach((i) => {
+      i.classList.remove("fade-out");
+      i.classList.add("fade-in");
+    });
   }
 });
